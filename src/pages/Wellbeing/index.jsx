9ca153883b3bc1dda0218550bd1d0181d17b76fc
Tomislav Dinev <dinevtomislav@gmail.com>
2023-06-12 16:14:32 +0100

    basic set up for the wellbeing page

--- src/pages/Wellbeing/index.jsx
+++ src/pages/Wellbeing/index.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Wellbeing = () => {
   return (
-    <div>Wellbeing</div>
+    <div className="wellbeing-page">
+    <h2>Welcome to the Wellbeing Page!</h2>
+    <div className="carousel-container">
+        <div className="carousel">
+        </div>
+    </div>
+    <div>
+      <Link to="journal">
+        <div className="box1">
+        <h3>Journal</h3>
+        <img src="" alt="" width={100} height={100} />
+        </div>
+      </Link>
+
+
+      <Link to="chat">
+        <div className="box2">
+        <h3>Chat with the therapist</h3>
+        <img src="" alt="" width={100} height={100} />
+        </div>
+      </Link>
+
+
+      <Link to="blogs">
+        <div className="box3">
+        <h3>Blogs</h3>
+        <img src="" alt="" width={100} height={100} />
+        </div>
+      </Link>
+
+
+    </div>
+    </div>
   )
 }
 
