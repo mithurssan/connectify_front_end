3d851ce64e046284c479c391e58caa2e5cb171ea
Tom Burzynski <tellur.beryl@gmail.com>
2023-06-13 14:18:23 +0100

    finish signup pages for user and business

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import * as Pages from "./pages"
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import * as Pages from './pages';
 import { NavBar } from './components';
-import "./App.css"
+import './App.css';
 
 const App = () => {
-  return (
-    <Routes>
-      <Route>
-        <Route path="/home" element={<Pages.Home />} />
-        <Route path="/" element={<NavBar />}>
-          <Route path="/rota" element={<Pages.Rota />} />
-          <Route path="/wellbeing" element={<Pages.Wellbeing />} />
-          <Route path="/dashboard" element={<Pages.Dashboard />} />
-          <Route path="/profile/:username" element={<Pages.Profile />} />
-          <Route path="/login" element={<Pages.Login />} />
-          <Route path="/signup" element={<Pages.Signup />} />
+	return (
+		<Routes>
+			<Route>
+				<Route path="/home" element={<Pages.Home />} />
+				<Route path="/" element={<NavBar />}>
+					<Route path="/rota" element={<Pages.Rota />} />
+					<Route path="/wellbeing" element={<Pages.Wellbeing />} />
+					<Route path="/dashboard" element={<Pages.Dashboard />} />
+					<Route path="/profile/:username" element={<Pages.Profile />} />
+					<Route path="/login/user" element={<Pages.LoginUser />} />
+					<Route path="/login/business" element={<Pages.LoginBusiness />} />
+					<Route path="/signup/business" element={<Pages.SignupBusiness />} />
+					<Route path="/signup/user" element={<Pages.SignupUser />} />
 
-          <Route path="*" element={<Pages.NotFound />} />
-        </Route>
-      </Route>
-    </Routes>
-  )
-}
+					<Route path="*" element={<Pages.NotFound />} />
+				</Route>
+			</Route>
+		</Routes>
+	);
+};
 
-export default App
+export default App;
