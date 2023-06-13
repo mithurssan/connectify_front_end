3d8da5c8b53448a1a7e7a0da882175a610588e43
Tomislav Dinev <dinevtomislav@gmail.com>
2023-06-13 12:08:02 +0100

    100 % coverage on Wellbeing

--- src/pages/Wellbeing/index.jsx
+++ src/pages/Wellbeing/index.jsx
@@ -1,73 +1,43 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import image1 from "../../assets/image 27.png"
-import journal from "../../assets/image 26.png"
-import chat from "../../assets/image 22.png"
-import blog from "../../assets/image 24.png"
+import image from "../../assets/image 27.png";
+import journal from "../../assets/image 26.png";
+import chat from "../../assets/image 22.png";
+import blog from "../../assets/image 24.png";
 
 const Wellbeing = () => {
-  const carouselSettings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
-
   return (
     <div className="wellbeing-page">
-      <h2>Mental Health</h2>
-      <p>Your well-being is important! Strive for a healthy balance between work, personal life, and leisure activities. </p>
-      <div className="carousel-container">
-        <Slider {...carouselSettings}>
-          <div>
-            <img src={image1} alt="" />
-          </div>
-          <div>
-            <img src={image1} alt="" />
-          </div>
-          <div>
-            <img src={image1} alt="" />
-          </div>
-          <div>
-            <img src={image1} alt="" />
-          </div>
-          <div>
-            <img src={image1} alt="" />
-          </div>
-        </Slider>
+      <h1>Mental Health</h1>
+      <p>Your well-being is important! Strive for a healthy balance between work, personal life, and leisure activities.</p>
+      <div className="image-container">
+        <img src={image} alt="Importance of mental health" />
       </div>
-    <div>
-      <Link to="journal">
-        <div className="box1">
-        <h3>Journal</h3>
-        <img src={journal} alt="" width={100} height={100} />
-        </div>
-      </Link>
+      <div>
+        <Link to="journal">
+          <div className="box1">
+            <h3>Journal</h3>
+            <img src={journal} alt="Journal" width={100} height={100} />
+          </div>
+        </Link>
 
+        <Link to="chat">
+          <div className="box2">
+            <h3>Chat with the therapist</h3>
+            <img src={chat} alt="Chat with the therapist" width={100} height={100} />
+          </div>
+        </Link>
 
-      <Link to="chat">
-        <div className="box2">
-        <h3>Chat with the therapist</h3>
-        <img src={chat} alt="" width={100} height={100} />
-        </div>
-      </Link>
-
-
-      <Link to="blogs">
-        <div className="box3">
-        <h3>Blogs</h3>
-        <img src={blog} alt="" width={100} height={100} />
-        </div>
-      </Link>
-
-
+        <Link to="blogs">
+          <div className="box3">
+            <h3>Blogs</h3>
+            <img src={blog} alt="Blogs" width={100} height={100} />
+          </div>
+        </Link>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Wellbeing
+export default Wellbeing;
+
--- src/pages/Wellbeing/index.test.jsx
+++ src/pages/Wellbeing/index.test.jsx
@@ -1,1 +1,62 @@
+import React from "react";
+import { getByRole, getByTestId,render, screen, cleanup } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { describe, it, expect, beforeEach, afterEach, vi } from "vitest";
+import matchers from "@testing-library/jest-dom/matchers";
+expect.extend(matchers);
+import Wellbeing from ".";
+
+describe("Wellbeing", () => {
+  beforeEach(() => {
+    render(
+      <BrowserRouter>
+        <Wellbeing />
+      </BrowserRouter>
+    );
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  it("renders the page heading", () => {
+    const heading = screen.getByRole('heading', { name: /mental health/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("renders the page description", () => {
+    const pageDescription = screen.getByText(
+      "Your well-being is important! Strive for a healthy balance between work, personal life, and leisure activities."
+    );
+    expect(pageDescription).toBeInTheDocument();
+  });
+
+  it("renders the image", () => {
+    const image = screen.getByAltText("Importance of mental health");
+    expect(image).toBeInTheDocument();
+  });
+
+  it("renders the Journal box with link", () => {
+    const journalBox = screen.getByText("Journal");
+    const journalImage = screen.getByAltText("Journal");
+    expect(journalBox).toBeInTheDocument();
+    expect(journalImage).toBeInTheDocument();
+  });
+
+  it("renders the Chat box with link", () => {
+    const chatBox = screen.getByText("Chat with the therapist");
+    const chatImage = screen.getByAltText("Chat with the therapist");
+    expect(chatBox).toBeInTheDocument();
+    expect(chatImage).toBeInTheDocument();
+  });
+
+  it("renders the Blogs box with link", () => {
+    const blogsBoxes = screen.getAllByText("Blogs");
+    const blogsImages = screen.getAllByAltText("Blogs");
+    expect(blogsBoxes.length).toBe(1);
+    expect(blogsImages.length).toBe(1);
+  });
+});
+
+
 
