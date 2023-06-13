13712c47daba3283f0f811bbe522671e5339e978
Tomislav Dinev <dinevtomislav@gmail.com>
2023-06-13 17:31:58 +0100

    added logic and drag and drop to rota page

--- src/pages/Rota/index.jsx
+++ src/pages/Rota/index.jsx
@@ -1,28 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop'; 
 import "./style.css"
 
 const localizer = momentLocalizer(moment);
+const DragAndDropCalendar = withDragAndDrop(Calendar); 
 
 const Rota = () => {
-  const events = [
-    {
-      title: 'Shift 1',
-      start: new Date(2023, 5, 13, 9, 0), // June 13, 2023, 09:00 AM
-      end: new Date(2023, 5, 13, 17, 0), // June 13, 2023, 05:00 PM
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    fetchEvents()
+      .then((data) => {
+        setEvents(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching events:', error);
+      });
+  }, []);
+
+  const fetchEvents = async () => {
+    try {
+      const response = await fetch('');
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      throw new Error('');
     }
-  ];
+  };
+
+  const handleEventDrop = async ({ event, start, end }) => {
+    const updatedEvent = { ...event, start, end };
+
+    try {
+      await updateEvent(updatedEvent);
+
+      const updatedEvents = events.map((ev) =>
+        ev.id === updatedEvent.id ? updatedEvent : ev
+      );
+      setEvents(updatedEvents);
+    } catch (error) {
+      console.error('Error updating event:', error);
+    }
+  };
 
   return (
     <div className="calendar-container">
       <div className="calendar-wrapper">
-        <Calendar
+        <DragAndDropCalendar
           localizer={localizer}
           events={events}
           startAccessor="start"
           endAccessor="end"
+          onEventDrop={handleEventDrop}
+          resizable
+          selectable
         />
       </div>
     </div>
