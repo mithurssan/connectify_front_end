14b32d8c341433550d0f73ab47fa94b5965fb392
Dohee Lee <dojuhee2015@gmail.com>
2023-06-13 13:08:50 +0100

    sidebar

---
+++ src/components/SidebarData/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import * as AiIcons from 'react-icons/ai'
+export const SidebarData = [
+  {
+    title: 'Dashboard',
+    path: '/',
+    icon: <AiIcons.AiFillHome />,
+    cName: 'nav-text',
+  },
+  {
+    title: 'My Rotas',
+    path: '/rota',
+    icon: <AiIcons.AiFillCalendar />,
+    cName: 'nav-text',
+  },
+]
