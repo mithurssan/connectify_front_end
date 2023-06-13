6b1341dbc2294d4270a46cdca7e9156a13e5356b
Tomislav Dinev <dinevtomislav@gmail.com>
2023-06-13 16:42:23 +0100

    addition of calendar on rota page

--- src/pages/Rota/index.jsx
+++ src/pages/Rota/index.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import "./style.css"
+
+const localizer = momentLocalizer(moment);
 
 const Rota = () => {
-  return (
-    <div>Rota</div>
-  )
-}
+  const events = [
+    {
+      title: 'Shift 1',
+      start: new Date(2023, 5, 13, 9, 0), // June 13, 2023, 09:00 AM
+      end: new Date(2023, 5, 13, 17, 0), // June 13, 2023, 05:00 PM
+    }
+  ];
 
-export default Rota
+  return (
+    <div className="calendar-container">
+      <div className="calendar-wrapper">
+        <Calendar
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Rota;
+
