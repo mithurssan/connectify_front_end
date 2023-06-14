cf279e0de61b0a03ff6f4b09577d91e3d79b8879
Tomislav Dinev <dinevtomislav@gmail.com>
2023-06-14 12:18:38 +0100

    added features and css to notfound page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import "./style.css"
 
 const NotFound = () => {
+  const goBack = () => {
+    window.history.back();
+  };
   return (
-    <div>NotFound</div>
+    <div className="not-found">
+      <h1 className="not-found-title">404 - Page Not Found</h1>
+      <p className="not-found-message">
+        Sorry, the page you're looking for does not exist.
+      </p>
+      <Link to="/" className="not-found-button">Go Back to Main Page</Link>
+      <button onClick={goBack} className="not-found-button">Go Back</button>
+    </div>
   )
 }
 
