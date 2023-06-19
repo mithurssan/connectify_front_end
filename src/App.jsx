40b46bfb917140f5044ca81ccf4d84d572dbe409
mithurssan <126930607+mithurssan@users.noreply.github.com>
2023-06-19 10:59:50 +0100

    remove unnecessary lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import * as Pages from './pages'
@@ -6,33 +5,33 @@ import { NavBar, LoginPage, LoginUse, Spinner } from './components'
 import useToken from './components/useToken'
 import './App.css'
 const App = () => {
-  const { token, setToken, removeToken } = useToken()
-  return (
-    <>
-      <Routes>
-        <Route path='/' element={<NavBar token={removeToken} />}>
-          <Route index element={<Pages.Dashboard />} />
-          <Route path='/rota' element={<Pages.Rota />} />
-          <Route path='/wellbeing' element={<Pages.Wellbeing />} />
-          <Route index element={<Pages.Dashboard />} />
-          <Route path='/profile/:username' element={<Pages.Profile />} />
-          <Route path='/bookings' element={<Pages.Booking />} />
-          <Route path='*' element={<Pages.NotFound />} />
-        </Route>
-        <Route path='/Home' element={<Pages.Home />} />
-        <Route path='/login/user' element={<Pages.LoginUser />} />
-        <Route path='/login/business' element={<Pages.LoginBusiness />} />
-        <Route path='/signup/business' element={<Pages.SignupBusiness />} />
-        <Route path='/signup/user' element={<Pages.SignupUser />} />
-        <Route
-          path='/login-register'
-          element={<LoginPage setToken={setToken} />}
-        />
-        <Route path='/login-user' element={<LoginUse setToken={setToken} />} />
-        <Route path='/spinner' element={<Spinner />} />
-      </Routes>
-    </>
-  )
+	const { token, setToken, removeToken } = useToken()
+	return (
+		<>
+			<Routes>
+				<Route path='/' element={<NavBar token={removeToken} />}>
+					<Route index element={<Pages.Dashboard />} />
+					<Route path='/rota' element={<Pages.Rota />} />
+					<Route path='/wellbeing' element={<Pages.Wellbeing />} />
+					<Route index element={<Pages.Dashboard />} />
+					<Route path='/profile/:username' element={<Pages.Profile />} />
+					<Route path='/bookings' element={<Pages.Booking />} />
+					<Route path='*' element={<Pages.NotFound />} />
+				</Route>
+				<Route path='/Home' element={<Pages.Home />} />
+				<Route path='/login/user' element={<Pages.LoginUser />} />
+				<Route path='/login/business' element={<Pages.LoginBusiness />} />
+				<Route path='/signup/business' element={<Pages.SignupBusiness />} />
+				<Route path='/signup/user' element={<Pages.SignupUser />} />
+				<Route
+					path='/login-register'
+					element={<LoginPage setToken={setToken} />}
+				/>
+				<Route path='/login-user' element={<LoginUse setToken={setToken} />} />
+				<Route path='/spinner' element={<Spinner />} />
+			</Routes>
+		</>
+	)
 }
 export default App
 =======
@@ -90,4 +89,3 @@ const App = () => {
 };
 
 export default App;
->>>>>>> 26ebd3d2bad74e1cff9e2ab3eca50fef02cc8d6e
