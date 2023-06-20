bdc40467d6dee2114b88e31a489d2e78e508c701
Tom Burzynski <tellur.beryl@gmail.com>
2023-06-20 16:49:22 +0100

    try to get chat up and running in prod

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import requireTransform from 'vite-plugin-require-transform';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		requireTransform({
+			fileRegex: /.jsx?$/,
+			importPrefix: '_vite_plugin_require_transform_',
+			importPathHandler: (requireSpecifier) => {
+				if (requireSpecifier === 'react-chat-engine') {
+					return 'react-chat-engine/dist/index.js';
+				}
+				return requireSpecifier;
+			},
+		}),
+	],
 	test: {
 		globals: true,
 		environment: 'jsdom',
