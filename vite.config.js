2d20e7a6e5fd778eaf6d24483bf26f304b1219d3
Tom Burzynski <tellur.beryl@gmail.com>
2023-06-20 12:52:28 +0100

    edit vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,17 @@ import requireTransform from 'vite-plugin-require-transform';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), requireTransform({})],
+	plugins: [
+		react(),
+		requireTransform({
+			transforms: [
+				{
+					test: /react-chat-engine/,
+					transform: 'react-chat-engine/esm',
+				},
+			],
+		}),
+	],
 	test: {
 		globals: true,
 		environment: 'jsdom',
