721996459b003e9660b0e44ac000affe5becb45d
mithurssan <126930607+mithurssan@users.noreply.github.com>
2023-06-22 19:35:51 +0100

    remove chat for deployment

--- src/pages/Chat/index.jsx
+++ src/pages/Chat/index.jsx
@@ -1,69 +1,69 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
-import { useSelector, useDispatch } from 'react-redux';
-import axios from 'axios';
-import './style.css';
-import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced';
-import { setPassword, setCompanyPassword } from '../../actions';
+// import { useSelector, useDispatch } from 'react-redux';
+// import axios from 'axios';
+// import './style.css';
+// import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced';
+// import { setPassword, setCompanyPassword } from '../../actions';
 
-const Chat = () => {
-	const dispatch = useDispatch();
-	const username = useSelector((state) => state.user.username);
-	const companyName = useSelector((state) => state.business.companyName);
-	// const [secret, setSecret] = useState('');
-	const secretUser = useSelector((state) => state.user.password);
-	const secretBusiness = useSelector((state) => state.business.companyPassword);
+// const Chat = () => {
+// 	const dispatch = useDispatch();
+// 	const username = useSelector((state) => state.user.username);
+// 	const companyName = useSelector((state) => state.business.companyName);
+// 	// const [secret, setSecret] = useState('');
+// 	const secretUser = useSelector((state) => state.user.password);
+// 	const secretBusiness = useSelector((state) => state.business.companyPassword);
 
-	const id = '7f8e7fee-521a-4f50-8d9a-9028fc529c34';
-	const isBusiness = localStorage.getItem('isBusiness');
+// 	const id = '7f8e7fee-521a-4f50-8d9a-9028fc529c34';
+// 	const isBusiness = localStorage.getItem('isBusiness');
 
-	isBusiness ? getCompanies() : getUsers();
+// 	isBusiness ? getCompanies() : getUsers();
 
-	async function getCompanies() {
-		try {
-			const url = 'http://127.0.0.1:5000/businesses/';
-			const res = await axios.get(url);
-			const data = await res.data;
+// 	async function getCompanies() {
+// 		try {
+// 			const url = 'http://127.0.0.1:5000/businesses/';
+// 			const res = await axios.get(url);
+// 			const data = await res.data;
 
-			const business = data.find((b) => b.business_name === companyName);
+// 			const business = data.find((b) => b.business_name === companyName);
 
-			if (business) {
-				dispatch(setCompanyPassword(business.business_password));
-			} else {
-				console.log('Business not found');
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	}
+// 			if (business) {
+// 				dispatch(setCompanyPassword(business.business_password));
+// 			} else {
+// 				console.log('Business not found');
+// 			}
+// 		} catch (error) {
+// 			console.log(error);
+// 		}
+// 	}
 
-	async function getUsers() {
-		try {
-			const url = 'http://127.0.0.1:5000/users/';
-			const res = await axios.get(url);
-			const data = await res.data;
+// 	async function getUsers() {
+// 		try {
+// 			const url = 'http://127.0.0.1:5000/users/';
+// 			const res = await axios.get(url);
+// 			const data = await res.data;
 
-			const user = data.find((u) => u.user_username === username);
+// 			const user = data.find((u) => u.user_username === username);
 
-			if (user) {
-				dispatch(setPassword(user.user_password));
-				console.log('User password:', user.user_password);
-			} else {
-				console.log('User not found');
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	}
+// 			if (user) {
+// 				dispatch(setPassword(user.user_password));
+// 				console.log('User password:', user.user_password);
+// 			} else {
+// 				console.log('User not found');
+// 			}
+// 		} catch (error) {
+// 			console.log(error);
+// 		}
+// 	}
 
-	const chatProps = useMultiChatLogic('7f8e7fee-521a-4f50-8d9a-9028fc529c34', isBusiness ? companyName : username, isBusiness ? secretBusiness : secretUser);
+// 	const chatProps = useMultiChatLogic('7f8e7fee-521a-4f50-8d9a-9028fc529c34', isBusiness ? companyName : username, isBusiness ? secretBusiness : secretUser);
 
-	return (
-		<>
-			<MultiChatWindow {...chatProps} />
-			<MultiChatSocket {...chatProps} />
-		</>
-	);
-};
+// 	return (
+// 		<>
+// 			<MultiChatWindow {...chatProps} />
+// 			<MultiChatSocket {...chatProps} />
+// 		</>
+// 	);
+// };
 
-export default Chat;
+// export default Chat;
