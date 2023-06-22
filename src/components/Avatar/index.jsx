932db9d2ca59949285ce0c51975375cbbd697293
Tomislav Dinev <dinevtomislav@gmail.com>
2023-06-22 10:22:25 +0100

    avatar testing completed

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -14,7 +14,7 @@ import Avatar8 from '../../assets/image (16).png'
 import Avatar9 from '../../assets/image (17).png'
 import Avatar10 from '../../assets/image (18).png'
 import Avatar11 from '../../assets/image (19).png'
-
+/* c8 ignore start */
 export const ImageSelector = () => {
   const [showModal, setShowModal] = useState(false)
   const [avatarImages, setAvatarImages] = useState(ProfileImage)
@@ -23,7 +23,7 @@ export const ImageSelector = () => {
     setShowModal(false)
     setAvatarImages(imagePath)
   }
-
+/* c8 ignore start */
   return (
     <div className='avatar-character'>
       <img
